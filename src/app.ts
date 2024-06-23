b475834ffbc300258105784c5fc07f737555615b
Rishav Sharma <rishavtosharma@gmail.com>
2024-06-23 20:32:44 +0530

    ci(docker): Add docker and containerize

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import { HttpError } from "http-errors";
 const app = express();
 
 /**
- * If you make any route handler async, then our global error handler middleware will not be able to catch the error thrown from the async route handler. To solve this issue, instead of using "throw err" we need to make use of the next() function and pass the error to it as a parameter "next(err)", so that the error is passed to the global error handler middleware by the next() function.
+ * If you make any route handler async, then our global error handler middleware will not be able to catch the error thrown from the async route handler. To solve this issue, instead of using "throw err" we need to make use of the next() function and pass the error to it as a parameter "next(err)", so that the error is passed to the global error handler middleware by the next() function
  */
 
 app.get("/", async (req, res) => {

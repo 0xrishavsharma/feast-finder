fddea690ea2b7cde2f495d30b6de26395c41b853
Rishav Sharma <rishavtosharma@gmail.com>
2024-07-24 14:39:02 +0530

    test(register): add test to check user persistence in db after register route

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,12 +1,38 @@
+// import { config as dotenvConfig } from "dotenv";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import { cleanEnv, num, str } from "envalid";
+
+// const __dirnameConfig = path.dirname(fileURLToPath(import.meta.url));
+
+// dotenvConfig({
+//     path: path.join(__dirnameConfig, `../../.env.${process.env.NODE_ENV}`),
+// });
+
+// const env = cleanEnv(process.env, {
+//     PORT: num(),
+//     NODE_ENV: str({ choices: ["development", "test", "production"] }),
+//     DB_HOST: str(),
+//     DB_PORT: num(),
+//     DB_USERNAME: str(),
+//     DB_PASSWORD: str(),
+//     DB_NAME: str(),
+// });
+
+// export default env;
+
+// Revised approach to avoid direct use of import.meta.url in a way that causes issues with tests
 import { config as dotenvConfig } from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
 import { cleanEnv, num, str } from "envalid";
 
-const __dirnameConfig = path.dirname(fileURLToPath(import.meta.url));
+// Assuming process.cwd() returns the project root directory correctly in your test environment
+// Adjust the relative path as necessary to point to your .env file correctly
+const rootDir = process.cwd();
+const envPath = path.join(rootDir, `.env.${process.env.NODE_ENV}`);
 
 dotenvConfig({
-    path: path.join(__dirnameConfig, `../../.env.${process.env.NODE_ENV}`),
+    path: envPath,
 });
 
 const env = cleanEnv(process.env, {
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,13 +1,15 @@
 // Bringing all the services code related to db here, so that down the line if we plan to switch our db, it will be easier to replace code related to db from a single file. And as a side effect, the controller will be free from low level and db specific queries
 
-import { AppDataSource } from "../config/data-source";
+import { Repository } from "typeorm";
 import { User } from "../entity/User";
 import { UserData } from "../types";
 
 export class UserService {
+    constructor(private userRepository: Repository<User>) {
+        this.userRepository = userRepository;
+    }
     async create({ firstName, lastName, email, password }: UserData) {
-        const userRepository = AppDataSource.getRepository(User);
-        await userRepository.save({
+        await this.userRepository.save({
             firstName,
             lastName,
             email,
